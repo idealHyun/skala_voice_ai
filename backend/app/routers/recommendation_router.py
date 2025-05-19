4596130a9f33d018762b44325ba881a81474b565
SanghyunLee <lgw9736@naver.com>
2025-05-20 07:50:04 +0900

    fix: .env 경로 수정

--- backend/app/routers/recommendation_router.py
+++ backend/app/routers/recommendation_router.py
@@ -7,7 +7,8 @@ from pydantic import BaseModel
 from typing import List
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
 
 class Recommendation(BaseModel):
     label: str
